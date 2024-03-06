7fd6cb18793c64bc6eb65b5bade1508ee78799a3
Ken Hall <ken.halljunior3@yahoo.com>
2024-03-06 09:52:42 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
 
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+//Welcoming header
+function Welcome() {
+  return <h1>Welcome To The College Basketball Data Website</h1>;
+}
+
+// Takes data for one school
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div>
+        <h2>{school}</h2>
+        <h3>Mascot: {name}</h3>
+        <h3>
+          Location: {city}, {state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+// Creates the team list
+function TeamList() {
+  return (
+    <div>
+      {teamsData.teams.map((team: TeamProps, index: number) => (
+        <Team key={index} {...team} />
+      ))}
+    </div>
+  );
+}
+
+//App function
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
